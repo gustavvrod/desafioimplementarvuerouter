ef5db1f5f66ba56f598dc8ff48f20aa770d25aac
gustavvrod <tabo_27_@hotmail.com>
2021-09-11 19:22:36 -0300

    tercer commit

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:gustavvrod/desafioimplementarvuerouter.git master:gh-pages
+git push -f git@github.com:gustavvrod/desafioimplementarvuerouter.git main:gh-pages
 cd -
